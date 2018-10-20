e6bcfe763894ac588d77a274132885ed7a8e2cdb
mgreim3373 <mgreim3373@gmail.com>
2018-10-20 14:56:54 -0400

    Update animations -Updated view page slide animations to include keyframe animations that adjust the position of the background images during page transitions. Background images are offset on the home page and are centered on the hospital and government pages. -Added a keyframe animation to the opening of the explore options modal and its child divs. -Updated body font to Josefin Sans.

--- js/app.js
+++ js/app.js
@@ -7,28 +7,29 @@ $("[data-js-page]").on("touchend", function(){
 	if(goal_page.includes('gov')) {
 		var gov_image = $("[data-page='gov_left']")
 		showGoalPage('hospit_right','slide_right', goal_page)
-		recenterImage(gov_image)
+		centerGovImageFunc()
 	} else if(goal_page.includes('right')) {
 		var hospit_image = $("[data-page='hospit_right']")
 		showGoalPage('gov_left', 'slide_left', goal_page)
-		recenterImage(hospit_image)
+		centerHospitImageFunc()
 	}
 })
 
 $("[data-js-page='home']").on("touchend", function(){
 	var goal_page = $(this).data("js-page")
-	var hospit_image = [$("[data-page='hospit_right']"), "-250px"]
-	var gov_image = [$("[data-page='gov_left']"), "250px"]
 	$(".page").removeClass("active slide_right slide_left")
 	$(".flex-container > div").css({"display":"initial"})
-	$(".option_container").addClass('hide')
-	offsetImage(hospit_image[0], hospit_image[1])
-	offsetImage(gov_image[0], gov_image[1])
-	(".option_container").css(({"display":"initial"}))
+	offsetGovImageFunc()
+	offsetHospitImageFunc()
+	$(".option_container").removeClass('explorePop')
+	$(".inner_container").children().removeClass('explorePop')
 })
 
 $(".explore_button").on("touchend", function(){
-	$(".option_container").removeClass('hide')
+	$(".option_container").addClass('explorePop')
+	setTimeout(() => {
+    $(".inner_container").children().addClass('explorePop')
+}, 100);
 })
 
 })
@@ -51,6 +52,23 @@ const recenterImage = (goalBackgroundImage) => {
 	goalBackgroundImage.css({"background-position":"0px"})
 }
 
-const offsetImage = (goalBackgroundImage, offsetValue) => {
-	goalBackgroundImage.css({"background-position":offsetValue})
+const offsetHospitImageFunc = () => {
+	$("[data-page='hospit_right']").addClass('offsetHospitImage')
+	$("[data-page='hospit_right']").removeClass('centerHospitImage')
+
+}
+const offsetGovImageFunc = () => {
+	console.log('hi')
+	$("[data-page='gov_left']").addClass('offsetGovImage')
+	$("[data-page='gov_left']").removeClass('centerGovImage')
+
+}
+
+const centerHospitImageFunc = () => {
+	$("[data-page='hospit_right']").addClass('centerHospitImage')
+
+}
+const centerGovImageFunc = () => {
+	$("[data-page='gov_left']").addClass('centerGovImage')
+
 }
