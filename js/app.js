5d114cb532aef8f82544c7d9d625a13d5ea89806
mgreim3373 <mgreim3373@gmail.com>
2018-10-19 21:30:24 -0400

    Create initial slide ui -Created a div containing the hospital page view -Created a div containing the government building view -Created a home view containing a 49% section of both the hospital and government divs, three navigation buttons, and three Flexbox boxes -Created functions to complete page transitions, and hide and show home view content

--- js/app.js
+++ js/app.js
@@ -1,10 +1,48 @@
 $(document).ready(function(){
 
-	$("[data-js-page]").on("touchend", function(){
-		var goal_page = $(this).data("js-page");
-		console.log('hi', goal_page)
-		$(".page").removeClass("active");
-		$(".page[data-page='"+goal_page+"']").addClass("active");
-	})
-
+$("[data-js-page]").on("touchend", function(){
+	var goal_page = $(this).data("js-page")
+	clearHomeContent()
+	clearActiveClass()
+	if(goal_page.includes('gov')) {
+		var gov_image = $("[data-page='gov_left']")
+		showGoalPage('hospit_right','slide_right', goal_page)
+		recenterImage(gov_image)
+	} else if(goal_page.includes('right')) {
+		var hospit_image = $("[data-page='hospit_right']")
+		showGoalPage('gov_left', 'slide_left', goal_page)
+		recenterImage(hospit_image)
+	}
 })
+
+$("[data-js-page='home']").on("touchend", function(){
+	var goal_page = $(this).data("js-page")
+	var hospit_image = [$("[data-page='hospit_right']"), "-250px"]
+	var gov_image = [$("[data-page='gov_left']"), "250px"]
+	$(".page").removeClass("active slide_right slide_left")
+	$(".flex-container > div").css({"display":"initial"})
+	offsetImage(hospit_image[0], hospit_image[1])
+	offsetImage(gov_image[0], gov_image[1])
+})
+})
+
+const clearActiveClass = () => {
+	$(".page").removeClass("active")
+}
+
+const showGoalPage = (currPage, currPageAction, goalPage) => {
+	$(".page[data-page='"+currPage+"']").addClass(currPageAction)
+	$(".page[data-page='"+goalPage+"']").addClass("active")
+}
+
+const clearHomeContent = () => {
+	$(".flex-container > div").css({"display":"none"})
+}
+
+const recenterImage = (goalBackgroundImage) => {
+	goalBackgroundImage.css({"background-position":"0px"})
+}
+
+const offsetImage = (goalBackgroundImage, offsetValue) => {
+	goalBackgroundImage.css({"background-position":offsetValue})
+}
