08897074b06b429297fc33cb314b36d3ab0a5b85
mgreim3373 <mgreim3373@gmail.com>
2018-10-21 18:49:18 -0400

    Updated styling and animations -Added keyframe animations to fade page items in and out on page transitions -Updated background colors -Added labels to the health page view -Added media queries to labels to make them responsive -Hid the explore modal button after the modal appears -Set max width and max height properties for page items -Updated font sizing on all items

--- js/app.js
+++ js/app.js
@@ -3,24 +3,25 @@ $(document).ready(function(){
 	// Page navigation functions start
 	$("[data-js-page]").on("touchend", function(){
 			var goal_page = $(this).data("js-page")
-			clearEntryContent()
 			clearActiveClass()
 
-			// If clicked button is government, show government page
+			// If clicked, button is government, show government page
 			if(goal_page.includes('gov')) {
+				clearEntryContent()
 				showGoalPage('health_outer_container','slide_right', goal_page)
 				centerGovImage()
-			// If clicked button is health, show health page
+			// If clicked, button is health, show health page
 			} else if(goal_page.includes('health')) {
+				clearEntryContent()
 				showGoalPage('gov_outer_container', 'slide_left', goal_page)
 				centerHealthImage()
-			// If clicked button is entry, show entry page
+			// If clicked, button is entry, show entry page
 			} else {
 					$(".page").removeClass("slide_right slide_left")
 					showEntryContent()
 					offsetGovImage()
 					offsetHealthImage()
-					clearModalAnimation()
+					clearNonGoalPage()
 			}
 		})
 		// Page navigation functions end
@@ -28,6 +29,7 @@ $(document).ready(function(){
 	// Explore modal function start
 	$(".gov_explore_button").on("touchend", function(){
 		$(".gov_option_container").addClass('explore_modal')
+		$(".gov_explore_button").addClass('fade_out')
 		setTimeout(() => {
 	    $(".gov_inner_container").children().addClass('explore_modal')
 		}, 100)
@@ -42,13 +44,22 @@ const clearActiveClass = () => {
 
 //Clear entry page content
 const clearEntryContent = () => {
-	$(".entry_flex_container > div").css({"display":"none"})
+	$(".entry_flex_container").addClass('fade_out')
+	setTimeout(() => {
+		$(".entry_flex_container > div").css({"display":"none"})
+		$(".entry_flex_container").removeClass('fade_out')
+	}, 400)
 }
 
 //Slide non-goal page view off screen and reveal goal page view
 const showGoalPage = (nonGoalPage, nonGoalPageAction, goalPage) => {
 	$(".page[data-page='"+nonGoalPage+"']").addClass(nonGoalPageAction)
 	$(".page[data-page='"+goalPage+"']").addClass("active")
+	if(goalPage.includes('gov')) {
+		$(".gov_home_button").addClass('fade_in')
+	} else {
+		$(".health_home_button, .option_lableA_container, .option_lableB_container").addClass('fade_in')
+	}
 }
 
 //Center goal-page background image position
@@ -60,9 +71,16 @@ const centerGovImage = () => {
 }
 
 //Clear explore modal content
-const clearModalAnimation = () => {
-	$(".gov_option_container").removeClass('explore_modal')
-	$(".gov_inner_container").children().removeClass('explore_modal')
+const clearNonGoalPage = () => {
+	$(".gov_inner_container").addClass('fade_out')
+	$(".gov_home_button").removeClass('fade_in')
+	$(".health_home_button, .option_lableA_container, .option_lableB_container").removeClass('fade_in')
+	setTimeout(() => {
+		$(".gov_option_container").removeClass('explore_modal')
+		$(".gov_inner_container").children().removeClass('explore_modal')
+		$(".gov_inner_container").removeClass('fade_out')
+		$(".gov_explore_button").removeClass('fade_out')
+	}, 400)
 }
 
 //Offset images for entry view
@@ -71,7 +89,6 @@ const offsetHealthImage = () => {
 	$("[data-page='health_outer_container']").removeClass('center_health_image')
 }
 const offsetGovImage = () => {
-	console.log('hi')
 	$("[data-page='gov_outer_container']").addClass('offset_gov_image')
 	$("[data-page='gov_outer_container']").removeClass('center_gov_image')
 }
